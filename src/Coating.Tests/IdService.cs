2729b14be028c0d7e22c0a08270121bca6efec63
Jens Andresen <jad@trifork.com>
2014-05-18 18:15:13 +0200

    added id service

---
+++ src/Coating.Tests/IdService.cs
@@ -0,0 +1,26 @@
+﻿namespace Coating.Tests
+{
+    public class IdService
+    {
+        private readonly string _propertyName;
+
+        public IdService() : this("Id")
+        {
+            
+        }
+
+        public IdService(string propertyName)
+        {
+            _propertyName = propertyName;
+        }
+
+        public string GetIdFrom(object o)
+        {
+            var idProperty = o
+                .GetType()
+                .GetProperty(_propertyName);
+
+            return idProperty.GetValue(o, null).ToString();
+        }
+    }
+}
---
+++ src/Coating.Tests/TestIdService.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace Coating.Tests
+{
+    [TestFixture]
+    public class TestIdService
+    {
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        [TestCase("Foo")]
+        [TestCase("Bar")]
+        [TestCase("Baz")]
+        [TestCase("Qux")]
+        [TestCase("with spaces")]
+        public void returns_value_of_id_property_by_default(string expected)
+        {
+            var sut = new IdService();
+            var actual = sut.GetIdFrom(new Foo {Id = expected});
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void can_use_alternative_property_identifier()
+        {
+            var foo = new Foo
+                {
+                    Id = "Foo",
+                    Key = "Bar"
+                };
+
+            var sut = new IdService("Key");
+            var actual = sut.GetIdFrom(foo);
+
+            Assert.AreEqual(foo.Key, actual);
+        }
+
+        private class Foo
+        {
+            public string Id { get; set; }
+            public string Key { get; set; }
+        }
+    }
+}
