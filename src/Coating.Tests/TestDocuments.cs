ef042c725e03f206ea10118f247ee0add0a13e1b
Jens Andresen <jad@trifork.com>
2014-05-19 11:26:05 +0200

    added test for documents

---
+++ src/Coating.Tests/TestDocuments.cs
@@ -0,0 +1,58 @@
+﻿using System.Data;
+using Moq;
+using NUnit.Framework;
+
+namespace Coating.Tests
+{
+    [TestFixture]
+    public class TestDocuments
+    {
+        [Test]
+        public void current_connection_returns_expected()
+        {
+            var expectedConnection = new Mock<IDbConnection>().Object;
+
+            var sut = new DocumentsBuilder()
+                .WithDbConnection(expectedConnection)
+                .Build();
+
+            Assert.AreSame(expectedConnection, sut.DbConnection);
+        }
+    }
+
+    public class DocumentsBuilder
+    {
+        private IDbConnection _dbConnection;
+
+        public DocumentsBuilder()
+        {
+            _dbConnection = new Mock<IDbConnection>().Object;
+        }
+
+        public DocumentsBuilder WithDbConnection(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+            return this;
+        }
+
+        public Documents Build()
+        {
+            return new Documents(_dbConnection);
+        }
+    }
+
+    public class Documents
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public Documents(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public IDbConnection DbConnection
+        {
+            get { return _dbConnection; }
+        }
+    }
+}
