41a2b1bf949e79a87a918e1a9401bf51b01338ae
Jens Andresen <jad@trifork.com>
2014-05-19 10:17:37 +0200

    added deserialize

--- src/Coating.Core/JsonSerializationService.cs
+++ src/Coating.Core/JsonSerializationService.cs
@@ -1,10 +1,50 @@
-﻿namespace Coating
+﻿using Newtonsoft.Json;
+
+namespace Coating
 {
     public class JsonSerializationService
     {
+        private readonly JsonSerializerSettings _settings;
+
+        public JsonSerializationService()
+        {
+            _settings = new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                    DateParseHandling = DateParseHandling.DateTime,
+                    DateTimeZoneHandling = DateTimeZoneHandling.Unspecified,
+                    DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
+                    Formatting = Formatting.None,
+                    MissingMemberHandling = MissingMemberHandling.Ignore,
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+        }
+
         public string Serialize(object o)
         {
-            return "";
+            if (o == null)
+            {
+                return "";
+            }
+
+            return JsonConvert.SerializeObject(o, _settings);
+        }
+
+        public T Deserialize<T>(string jsonData) where T : class 
+        {
+            if (string.IsNullOrWhiteSpace(jsonData))
+            {
+                return null;
+            }
+
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(jsonData, _settings);
+            }
+            catch (JsonReaderException)
+            {
+                return null;
+            }
         }
     }
 }
--- src/Coating.Tests/TestJsonSerializationService.cs
+++ src/Coating.Tests/TestJsonSerializationService.cs
@@ -24,9 +24,47 @@ namespace Coating.Tests
                     Bar = "2"
                 });
 
-            var expected = "{ \"Foo\": \"1\", \"Bar\": \"2\" }";
+            var expected = "{\"Foo\":\"1\",\"Bar\":\"2\"}";
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void returns_expected_when_deserializing_simple_object()
+        {
+            var sut = new JsonSerializationService();
+            var actual = sut.Deserialize<FooBar>("{\"Foo\":\"1\",\"Bar\":\"2\"}");
+            var expected = new FooBar
+                {
+                    Foo = "1",
+                    Bar = "2"
+                };
+
+            Assert.AreEqual(expected.Foo, actual.Foo);
+            Assert.AreEqual(expected.Bar, actual.Bar);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("invalid-json")]
+        public void returns_expected_when_deserializing_invalid_json(string jsonData)
+        {
+            var sut = new JsonSerializationService();
+            var actual = sut.Deserialize<FooBar>(jsonData);
+
+            Assert.IsNull(actual);
+        }
+
+        private class FooBar
+        {
+            public string Foo { get; set; }
+            public string Bar { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("Foo: {0}, Bar: {1}", Foo, Bar);
+            }
         }
     }
 }
