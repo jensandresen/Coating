c45be6b7ec8bf234eee5a0fc1586a45252121a79
Jens Andresen <jad@trifork.com>
2014-05-19 11:34:22 +0200

    WIP: end to end test

--- src/Coating.Tests/TestDocuments.cs
+++ src/Coating.Tests/TestDocuments.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Data.SqlClient;
 using Moq;
 using NUnit.Framework;
 
@@ -8,7 +9,7 @@ namespace Coating.Tests
     public class TestDocuments
     {
         [Test]
-        public void current_connection_returns_expected()
+        public void dbconnection_returns_expected()
         {
             var expectedConnection = new Mock<IDbConnection>().Object;
 
@@ -18,6 +19,60 @@ namespace Coating.Tests
 
             Assert.AreSame(expectedConnection, sut.DbConnection);
         }
+
+        [Test]
+        public void end_to_end_with_default_settings()
+        {
+        }
+
+        private class Foo
+        {
+            public string Id { get; set; }
+            public string Key { get; set; }
+        }
+    }
+
+    public class SpyDbConnection : IDbConnection
+    {
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDbTransaction BeginTransaction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDbTransaction BeginTransaction(IsolationLevel il)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ChangeDatabase(string databaseName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IDbCommand CreateCommand()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ConnectionString { get; set; }
+        public int ConnectionTimeout { get; private set; }
+        public string Database { get; private set; }
+        public ConnectionState State { get; private set; }
     }
 
     public class DocumentsBuilder
